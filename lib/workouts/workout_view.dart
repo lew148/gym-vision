33c5b24c4d9573940bbbfd378eab7d0d47993135
Lew <lewis.reid148@gmail.com>
2022-10-03 15:53:50 +0100

    add exercises to workout - wip

--- lib/workouts/workout_view.dart
+++ lib/workouts/workout_view.dart
@@ -16,6 +16,8 @@ class WorkoutView extends StatefulWidget {
 class _WorkoutViewState extends State<WorkoutView> {
   reloadState() => setState(() {});
 
+  void onAddExerciseClick() => null;
+
   @override
   Widget build(BuildContext context) {
     final Future<Workout> workout =
@@ -25,25 +27,46 @@ class _WorkoutViewState extends State<WorkoutView> {
       appBar: AppBar(
         title: Text(widget.workoutDateString),
       ),
-      body: FutureBuilder<Workout>(
-        future: workout,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: Text('Loading...'),
-            );
-          }
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 3),
+            child: Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: onAddExerciseClick,
+                    child: const Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Text(
+                        'Add Exercise',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Divider(),
+          FutureBuilder<Workout>(
+            future: workout,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: Text('Loading...'),
+                );
+              }
 
-          return Column(
-            children: [
-              Container(
+              return Container(
                 margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
                 child: Text(snapshot.data!.id.toString()),
-              ),
-              const Divider(),
-            ],
-          );
-        },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
