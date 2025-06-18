aafb326865c8d0b15e225eca592ca43aa51a99e2
Lew <lewis.reid148@gmail.com>
2025-06-18 19:47:46 +0100

    made the re-inserting of data reset while persiting data more granular

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -158,36 +158,90 @@ class DatabaseHelper {
       var newDb = await getDb();
 
       for (var w in workouts) {
-        await newDb.insert('workouts', w);
+        await newDb.insert('workouts', {
+          'id': w['id'],
+          'updatedAt': w['updatedAt'],
+          'createdAt': w['createdAt'],
+          'date': w['date'],
+        });
       }
 
       for (var weo in workoutExerciseOrderings) {
-        await newDb.insert('workout_exercise_orderings', weo);
+        await newDb.insert('workout_exercise_orderings', {
+          'id': weo['id'],
+          'updatedAt': weo['updatedAt'],
+          'createdAt': weo['createdAt'],
+          'workoutId': weo['workoutId'],
+          'positions': weo['positions'],
+        });
       }
 
       for (var wc in workoutCategories) {
-        await newDb.insert('workout_categories', wc);
+        await newDb.insert('workout_categories', {
+          'id': wc['id'],
+          'updatedAt': wc['updatedAt'],
+          'createdAt': wc['createdAt'],
+          'workoutId': wc['workoutId'],
+          'category': wc['category'],
+        });
       }
 
       for (var we in workoutExercises) {
-        await newDb.insert('workout_exercises', we);
+        await newDb.insert('workout_exercises', {
+          'id': we['id'],
+          'updatedAt': we['updatedAt'],
+          'createdAt': we['createdAt'],
+          'workoutId': we['workoutId'],
+          'exerciseIdentifier': we['exerciseIdentifier'],
+          'done': we['done'],
+        });
       }
 
       for (var ws in workoutSets) {
-        await newDb.insert('workout_sets', ws);
+        await newDb.insert('workout_sets', {
+          'id': ws['id'],
+          'updatedAt': ws['updatedAt'],
+          'createdAt': ws['createdAt'],
+          'workoutExerciseId': ws['workoutExerciseId'],
+          'done': ws['done'],
+          'weight': ws['weight'],
+          'reps': ws['reps'],
+          'time': ws['time'],
+          'distance': ws['distance'],
+          'calsBurned': ws['calsBurned'],
+        });
       }
 
       for (var fts in flavourTextSchedules) {
-        await newDb.insert('flavour_text_schedules', fts);
+        await newDb.insert('flavour_text_schedules', {
+          'id': fts['id'],
+          'updatedAt': fts['updatedAt'],
+          'createdAt': fts['createdAt'],
+          'flavourTextId': fts['flavourTextId'],
+          'date': fts['date'],
+          'dismissed': fts['dismissed'],
+        });
       }
 
       for (var bw in bodyweights) {
-        await newDb.insert('bodyweights', bw);
+        await newDb.insert('bodyweights', {
+          'id': bw['id'],
+          'updatedAt': bw['updatedAt'],
+          'createdAt': bw['createdAt'],
+          'weight': bw['weight'],
+          'date': bw['date'],
+          'units': bw['units'],
+        });
       }
 
       for (var us in userSettings) {
         // new record is made in initialDbCreate()
-        await newDb.update('user_settings', us);
+        await newDb.update('user_settings', {
+          'id': us['id'],
+          'updatedAt': us['updatedAt'],
+          'createdAt': us['createdAt'],
+          'theme': us['theme'],
+        });
       }
 
       return true;
